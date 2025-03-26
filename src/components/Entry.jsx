2f327d42d9625ee03be909e623350a986ddd0dc0
Gilbert Rodriguez <gilbert.jsin@gmail.com>
2025-03-25 21:43:10 -0500

    adding info container

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -6,6 +6,11 @@ function Entry(){
             <div className="entry-img-container">
                 <img src="src\assets\mt_fuji.jpg" className="country-img"/>
             </div>
+            <div className='entry-info'>
+                <div className='entry-info-container'>
+                    //TODO: fill out country info
+                </div>
+            </div>
 
         </article>
     )
