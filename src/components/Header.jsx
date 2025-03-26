390170372a3ebc0604de419a2f6046ca8bd137b7
Gilbert Rodriguez <gilbert.jsin@gmail.com>
2025-03-25 19:29:42 -0500

    built header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <header className="header">
+            <img src="../public/globe.png" alt="globe icon"/>
+            <span>React Facts</span>
+        </header>
+    )
+}
+
+export default Header;
