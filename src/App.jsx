a69556a34ccf4f35defafcf887cafa2a3809d50f
Gilbert Rodriguez <gilbert.jsin@gmail.com>
2025-04-01 20:29:47 -0500

    started adding props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './index.css';
 import Header from './components/Header';
 import Entry from './components/Entry';
+import marker from './assets/marker.png';
+import mtFuji from './assets/mt_fuji.jpg';
 
 function App() {
   return (
     <>
       <Header /> 
-      <Entry />     
+        <Entry 
+          locImg={{ src: mtFuji, alt: "Mt. Fuji" }}
+          markerImg={{ src: marker, alt: "Google Marker" }}  
+          countryName="Japan"  
+          mapLink="https://maps.app.goo.gl/7QmWeZUqyboNGZyy5"
+          locationName="Mount Fuji"
+          travelDates="12 Jan, 2021 - 24 Jan, 2021"
+          attractionFact="Mount Fuji is Japan’s highest mountain, standing at 3,776 meters 
+                          (12,389 feet), and is an active stratovolcano that last erupted in 1707.
+                          It is considered one of Japan’s “Three Holy Mountains” and is a symbol of
+                            national identity, art, and spirituality."/>   
     </>
   );
 }
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
 
-function Entry(){
+function Entry(props){
     return(
         <main className="entry-container">
             <article className="entry-article">
                 <div className="entry-img-container">
-                    <img src="src\assets\mt_fuji.jpg" className="country-img"/>
+                <img 
+                    src={props.locImg.src} 
+                    alt={props.locImg.alt} 
+                    className="country-img"
+                />
                 </div>
             </article>
             <article className="entry-info">
                 <div className="entry-info-map-link">
-                <img src="src\assets\marker.png" className="marker-img"/>
-                <span className="country-name">JAPAN</span>
-                <a href="https://maps.app.goo.gl/7QmWeZUqyboNGZyy5" className="google-map-link">View on Google Maps</a>
+                <img 
+                    src={props.markerImg.src} 
+                    alt={props.markerImg.alt} 
+                    className="marker-img"
+                />
+                <span className="country-name">{props.countryName}</span>
+                <a href={props.mapLink} className="google-map-link">View on Google Maps</a>
                 </div>
                 <div className="entry-info-container">
-                    <span className="attraction-location">Mount Fuji</span>
-                    <span className="visit-dates">12 Jan, 2021 - 24 Jan, 2021</span>
-                    <span className="attraction-facts">Mount Fuji is Japan’s highest mountain, standing at 3,776 meters 
-                        (12,389 feet), and is an active stratovolcano that last erupted in 1707.
-                         It is considered one of Japan’s “Three Holy Mountains” and is a symbol of
-                          national identity, art, and spirituality.</span>
-                </div>                
-            </article>
+                    <span className="attraction-location">{props.locationName}</span>
+                    <span className="visit-dates">{props.travelDates}</span>
+                    <span className="attraction-facts">{props.attractionFact}</span>
+                </div> 
+                </article>
         </main>
     )
 }
