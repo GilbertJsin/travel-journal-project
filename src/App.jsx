ea33622be21a46c40eb0b0f461ecbf64bdf2c353
Gilbert Rodriguez <gilbert.jsin@gmail.com>
2025-04-01 21:14:55 -0500

    added styling to journal entry component

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,18 @@ function App() {
           attractionFact="Mount Fuji is Japan’s highest mountain, standing at 3,776 meters 
                           (12,389 feet), and is an active stratovolcano that last erupted in 1707.
                           It is considered one of Japan’s “Three Holy Mountains” and is a symbol of
-                            national identity, art, and spirituality."/>   
+                            national identity, art, and spirituality."/> 
+                            <Entry 
+                              locImg={{ src: mtFuji, alt: "Mt. Fuji" }}
+                              markerImg={{ src: marker, alt: "Google Marker" }}  
+                              countryName="Japan"  
+                              mapLink="https://maps.app.goo.gl/7QmWeZUqyboNGZyy5"
+                              locationName="Mount Fuji"
+                              travelDates="12 Jan, 2021 - 24 Jan, 2021"
+                              attractionFact="Mount Fuji is Japan’s highest mountain, standing at 3,776 meters 
+                                              (12,389 feet), and is an active stratovolcano that last erupted in 1707.
+                                              It is considered one of Japan’s “Three Holy Mountains” and is a symbol of
+                                                national identity, art, and spirituality."/>      
     </>
   );
 }
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -3,31 +3,33 @@ import React from 'react';
 function Entry(props){
     return(
         <main className="entry-container">
-            <article className="entry-article">
-                <div className="entry-img-container">
-                <img 
-                    src={props.locImg.src} 
-                    alt={props.locImg.alt} 
-                    className="country-img"
-                />
-                </div>
-            </article>
-            <article className="entry-info">
-                <div className="entry-info-map-link">
-                <img 
-                    src={props.markerImg.src} 
-                    alt={props.markerImg.alt} 
-                    className="marker-img"
-                />
-                <span className="country-name">{props.countryName}</span>
-                <a href={props.mapLink} className="google-map-link">View on Google Maps</a>
-                </div>
-                <div className="entry-info-container">
-                    <span className="attraction-location">{props.locationName}</span>
-                    <span className="visit-dates">{props.travelDates}</span>
-                    <span className="attraction-facts">{props.attractionFact}</span>
-                </div> 
+            <div className="journal-entry">
+                <article className="entry-article">
+                    <div className="entry-img-container">
+                    <img 
+                        src={props.locImg.src} 
+                        alt={props.locImg.alt} 
+                        className="country-img"
+                    />
+                    </div>
                 </article>
+                <article className="entry-info">
+                    <div className="entry-info-map-link">
+                    <img 
+                        src={props.markerImg.src} 
+                        alt={props.markerImg.alt} 
+                        className="marker-img"
+                    />
+                    <span className="country-name">{props.countryName}</span>
+                    <a href={props.mapLink} className="google-map-link">View on Google Maps</a>
+                    </div>
+                    <div className="entry-info-container">
+                        <span className="attraction-location">{props.locationName}</span>
+                        <span className="visit-dates">{props.travelDates}</span>
+                        <span className="attraction-facts">{props.attractionFact}</span>
+                    </div> 
+                </article>
+            </div>
         </main>
     )
 }
