bbb189048e7e8adbcc9542536d73f54dee463865
Gilbert Rodriguez <gilbert.jsin@gmail.com>
2025-04-04 09:06:39 -0500

    pull data from data file in an array

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,28 @@ import './index.css';
 import Header from './components/Header';
 import Entry from './components/Entry';
 import marker from './assets/marker.png';
-import mtFuji from './assets/mt_fuji.jpg';
+import entryData from './data';
+
 
 function App() {
+
+  const journalEntries = entryData.map((entry) =>(
+    <Entry 
+        key={entry.id}
+        locImg={entry.img}
+        markerImg={{ src: marker, alt: "Google Marker" }}
+        countryName= {entry.countryName}
+        mapLink= {entry.mapLink}
+        locationName= {entry.title}
+        travelDates= {entry.travelDates}
+        attractionFact= {entry.text}      
+        /> 
+    ));
+
   return (
     <>
       <Header /> 
-        <Entry 
-          locImg={{ src: mtFuji, alt: "Mt. Fuji" }}
-          markerImg={{ src: marker, alt: "Google Marker" }}  
-          countryName="Japan"  
-          mapLink="https://maps.app.goo.gl/7QmWeZUqyboNGZyy5"
-          locationName="Mount Fuji"
-          travelDates="12 Jan, 2021 - 24 Jan, 2021"
-          attractionFact="Mount Fuji is Japan’s highest mountain, standing at 3,776 meters 
-                          (12,389 feet), and is an active stratovolcano that last erupted in 1707.
-                          It is considered one of Japan’s “Three Holy Mountains” and is a symbol of
-                            national identity, art, and spirituality."/> 
-                            <Entry 
-                              locImg={{ src: mtFuji, alt: "Mt. Fuji" }}
-                              markerImg={{ src: marker, alt: "Google Marker" }}  
-                              countryName="Japan"  
-                              mapLink="https://maps.app.goo.gl/7QmWeZUqyboNGZyy5"
-                              locationName="Mount Fuji"
-                              travelDates="12 Jan, 2021 - 24 Jan, 2021"
-                              attractionFact="Mount Fuji is Japan’s highest mountain, standing at 3,776 meters 
-                                              (12,389 feet), and is an active stratovolcano that last erupted in 1707.
-                                              It is considered one of Japan’s “Three Holy Mountains” and is a symbol of
-                                                national identity, art, and spirituality."/>      
+        {journalEntries} 
     </>
   );
 }
